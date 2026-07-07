901bd1ec5b0a861f7b6ec3fe5a1cba43bc1dd184
imtiazrazzak <imtiazrazzak@gmail.com>
2026-07-06 18:36:55 -0700

    feat(core): implement event bus

---
+++ Assets/Scripts/Core/Events/EventBus.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TopDownRPGFramework.Core.Events
+{
+    public sealed class EventBus : IEventBus
+    {
+        private readonly Dictionary<Type, Delegate> _handlers = new();
+        public void Publish<TEvent>(TEvent eventData) where TEvent : IEvent
+        {
+            Type eventType = typeof(TEvent);
+            if(_handlers.TryGetValue(eventType, out Delegate handlers))
+            {
+                ((Action<TEvent>)handlers)?.Invoke(eventData);
+            }
+        }
+
+        public void Subscribe<TEvent>(Action<TEvent> handler) where TEvent : IEvent
+        {
+            Type eventType = typeof(TEvent);
+
+            if(_handlers.TryGetValue(eventType, out Delegate existingHandlers))
+            {
+                _handlers[eventType] = Delegate.Combine(existingHandlers, handler);
+            }
+            else
+            {
+                _handlers[eventType] = handler;
+            }
+        }
+
+        public void Unsubscribe<TEvent>(Action<TEvent> handler) where TEvent : IEvent
+        {
+            Type eventType = typeof(TEvent);
+            if( _handlers.TryGetValue(eventType,out Delegate existingHandlers))
+            {
+                Delegate updateHandlers = Delegate.Remove(existingHandlers, handler);
+
+                if(updateHandlers == null)
+                {
+                    _handlers.Remove(eventType);
+                }
+                else
+                {
+                    _handlers[eventType] = updateHandlers;
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/Core/Events/IEventBus.cs
+++ Assets/Scripts/Core/Events/IEventBus.cs
@@ -7,6 +7,6 @@ namespace TopDownRPGFramework.Core.Events
         void Publish<TEvent>(TEvent eventData) where TEvent : IEvent;
         void Subscribe<TEvent>(Action<TEvent> handler) where TEvent : IEvent;
 
-        void Unsubscribe<TEvent>(Action<TEvent> handle) where TEvent : IEvent;
+        void Unsubscribe<TEvent>(Action<TEvent> handler) where TEvent : IEvent;
     }
 }
